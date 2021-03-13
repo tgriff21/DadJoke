b89f6067acd77cd9448ae1133d17296305ffb4b6
tgriff21 <74691411+tgriff21@users.noreply.github.com>
2021-03-12 22:29:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+// button.addEventListener('click', addNewJoke)
 
-
-
-
-
-
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+    
+}
 const getDadJoke = async () => {
     const config = {headers: {Accept: 'application/json'} }
     const res = await axios.get('https://icanhazdadjoke.com/', config)
-    console.log(res.data.joke)
+    return res.data.joke;
 
 }
+button.addEventListener('click', addNewJoke)
+
+
+
+
+
+
